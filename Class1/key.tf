924e2635e3c328bc4aa0176a60ce6c63a6a849ac
ulan8888 <tezjet888@gmail.com>
2023-09-26 23:36:57 +0000

    new files

--- Class1/key.tf
+++ Class1/key.tf
@@ -1,9 +1,27 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file("~/.ssh/id_rsa.pub")
-  }
+}
 
 
-  resource "aws_s3_bucket" "example" {
-    bucket_prefix = "hi-"
-  }
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "hi-"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "kaizen12345678910"
+}
+
+output "backet_info" {
+  value = [
+    aws_s3_bucket.bucket.bucket,
+    aws_s3_bucket.bucket.arn
+  ]
+}
+
+
+
+
+
+
+
